0fdd97b2a371e0bf77819060ec2f3bc9c43d23f1
karanbulani <ksbrocks.bulani@gmail.com>
2024-09-19 00:08:17 +0530

    config with 217

---
+++ 217 Contains Duplicate/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        num_values = set()
+        for num in nums:
+            if num in num_values:
+                return True
+            num_values.add(num)
+        return False
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1,1,1,3,3,4,3,2,4,2]  # Example input
+    result = solution.containsDuplicate(nums)
+    print(result)
