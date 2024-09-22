a0ad3fabb394d45dff4c3b279b9f1f87e6457b6b
karanbulani <ksbrocks.bulani@gmail.com>
2024-09-22 17:10:24 +0530

    242 Valid Anagram

---
+++ 242 Valid Anagram/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_count = {}
+        for char in s:
+            s_count[char] = 1 + s_count.get(char, 0)
+
+        t_count = {}
+        for char in t:
+            t_count[char] = 1 + t_count.get(char, 0)
+
+        for key in s_count:
+            if s_count[key] != t_count.get(key,0):
+                return False
+        return True
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "anagram"#"rat"
+    t = "nagaram"#"car"
+    result = solution.isAnagram(s, t)
+    print(result)
