6ee25ab843a5bdd54d11e9ab1f039e4ba1c111fc
karanbulani <ksbrocks.bulani@gmail.com>
2024-09-25 00:18:23 +0530

    49 Group Anagrams

--- 49 Group Anagrams/Solution.py
+++ 49 Group Anagrams/Solution.py
@@ -1,25 +1,4 @@
-'''
-Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-
-Example 1:
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
-Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-
-Explanation:
-There is no string in strs that can be rearranged to form "bat".
-The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
-The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
-
-Example 2:
-Input: strs = [""]
-Output: [[""]]
-
-Example 3:
-Input: strs = ["a"]
-Output: [["a"]]
-'''
 from collections import defaultdict
-
 
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
@@ -28,12 +7,15 @@ class Solution:
             keyArr = [0] * 26
             for char in word:
                 keyArr[ord(char) - 97] += 1
-            key = ''.join(map(str,keyArr)) #Or use key = tuple(keyArr)
+            #cant use ''.join(map(str,keyArr)) because
+            # 1b and 10d will lead to 0 1 0 10  and
+            # 10b 1c would lead to    0 10 1 0 which is same number
+            key = tuple(keyArr)
             grouped_dict[key].append(word)
         return grouped_dict.values()
 
 if __name__ == "__main__":
     solution = Solution()
-    strs = ["eat","tea","tan","ate","nat","bat"] #[""]#["a"]
+    strs = ["bdddddddddd","bbbbbbbbbbc"] #["eat","tea","tan","ate","nat","bat"] #[""]#["a"]
     result = solution.groupAnagrams(strs)
     print(result)
