fbf2a91becd9252e513e815f51fa13d1ab60db13
karanbulani <ksbrocks.bulani@gmail.com>
2024-09-25 23:59:07 +0530

    347 Top K Frequent Elements

---
+++ 347 Top K Frequent Elements/Solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count_dict = {}
+        for num in nums:
+            count_dict[num] = 1 + count_dict.get(num, 0)
+
+        count_bucket = [[] for _ in range(len(nums)) ]
+
+        for key, value in count_dict.items():
+            count_bucket[value - 1].append(key) ##when all 1 then count will be n but list goes from 0..n-1 hence v-1
+
+        res = []
+
+        for i in range(len(nums) - 1, -1, -1):
+            # this if is not needed as if count_bucket[i] is empty
+            # then for loop 2 line below anyways won't execute
+            if not count_bucket[i]:
+                continue
+            for value in count_bucket[i]:
+                res.append(value)# can use count_bucket[i].pop() instead of value but if count is same then does it even matter?
+                if len(res) == k:
+                    return res
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1,1,1,2,2,2,3,3]#[1]
+    k = 2 #1
+    result = solution.topKFrequent(nums, k)
+    print(result)
