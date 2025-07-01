6c9e7e0d2bb2cb0f89a9a708b92b6027dc5e24ca
karanbulani <ksbrocks.bulani@gmail.com>
2025-07-01 12:56:26 +0530

    Update gitignore 2

---
+++ 853 Car Fleet/Solution.py
@@ -0,0 +1,14 @@
+'''
+
+'''
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        return 10
+
+if __name__ == "__main__":
+    solution = Solution()
+    target0 = 12; position0 = [10,8,0,5,3]; speed0 = [2,4,1,1,3]
+    target1 = 10; position1 = [3]; speed1 = [3]
+    target2 = 100; position2 = [0,2,4]; speed2 = [4,2,1]
+    result = solution.carFleet(target0, position0, speed0)
+    print(result)
