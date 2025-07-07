47b847c3ecec943d68d75062283368d8f687dec1
karanbulani <ksbrocks.bulani@gmail.com>
2025-07-07 23:12:01 +0530

    853 solution

--- 853 Car Fleet/Solution.py
+++ 853 Car Fleet/Solution.py
@@ -1,14 +1,29 @@
 '''
-
+Time Complexity: O(nlogn) (sort + go through)
+Space Complexity: O(n) for stack
 '''
 class Solution:
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
-        return 10
+        pair = [ (p,s) for p, s in zip(position, speed)]
+        pair = sorted(pair, key = lambda x: x[0], reverse = True)
+
+        stack = []
+        for p,s in pair:
+            timeTaken = (target - p)/s
+            if not stack or stack[-1] < timeTaken:
+                stack.append(timeTaken)
+        return len(stack)
 
 if __name__ == "__main__":
     solution = Solution()
-    target0 = 12; position0 = [10,8,0,5,3]; speed0 = [2,4,1,1,3]
-    target1 = 10; position1 = [3]; speed1 = [3]
-    target2 = 100; position2 = [0,2,4]; speed2 = [4,2,1]
-    result = solution.carFleet(target0, position0, speed0)
-    print(result)
+
+    examples = [
+        # (target, position_list, speed_list) 3, 1, 1
+        (12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]),
+        (10, [3], [3]),
+        (100, [0, 2, 4], [4, 2, 1])
+    ]
+
+    for idx, (target, position, speed) in enumerate(examples, start=1):
+        result = solution.carFleet(target, position, speed)
+        print(f"Example {idx}: carFleet({target}, {position}, {speed}) -> {result}")
